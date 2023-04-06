4e3465e9142bbd5714ef115aec60794d445b8aa5
MASEPA12 <masepa1204@gmail.com>
2023-04-06 22:23:14 +0200

    Marina_animationsBase
    Vale, ja he posat es personatges i asignat ses animacions. Hi ha un script basic per veure es funcionament (NOplayercontroller). Es nin, com que crec que no l'utilitzarem, no he actualitzat ses animacions (he fet que hi hagui un pas intermig entre aixecat i crouched :)

---
+++ Assets/SCRIPTS/NOplayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NOplayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float walkingForce = 0.5f;
+   
+    public float jumpingForce = 0.2f;
+    private float gravityModifier = 1.5f;
+
+    //private bool isJumping;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        rb = GetComponent<Rigidbody>();
+        //isJumping = false;
+
+    }
+
+    private void Update()
+    {
+        bool crouchPressed = Input.GetKey("w");
+
+        if (crouchPressed)
+        {
+            animator.SetBool("isSteady", false); //ja no esta`dret 
+
+        }
+        if (!crouchPressed)
+        {
+            animator.SetBool("isSteady", true); //si no pitj, està dret
+            //rb.AddForce(Vector3.forward * walkingForce, ForceMode.Force);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) //bota
+        {
+            rb.AddForce(Vector3.up * jumpingForce, ForceMode.Impulse);
+
+            animator.SetBool("isJumping", true);
+
+        }
+
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isJumping", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
+
+
