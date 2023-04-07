f76e3c4b99a0d5fc892ce43ea39153a82e2233d8
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2023-04-07 17:23:30 +0200

    uncompleted PlayerMovement
    irregular playerMovement scripts and a testing ground to try diferents platforms types

---
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Script that controls the player movement, animation and 
+interaction with other object
+ */
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float walkingForce = 0.5f;
+
+    public float jumpingForce = 0.2f;
+    private float gravityModifier = 1.5f;
+
+    //Speed variables
+    private float forwardInput; //Forward and backwar move
+    private float horizontalInput; //Rotation movement
+    public float rotationSpeed = 6.5f; //Rotation speed
+
+    //private bool isJumping;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        rb = GetComponent<Rigidbody>();
+        //isJumping = false;
+
+    }
+
+    private void Update()
+    {
+        bool crouchPressed = Input.GetKey("c");
+        forwardInput = Input.GetAxis("Vertical"); //Check if W or S is pressed
+        horizontalInput = Input.GetAxis("Horizontal"); //Check if A o D is pressed
+
+        if (crouchPressed)
+        {
+            animator.SetBool("isSteady", false); //ja no esta`dret 
+
+        }
+        if (!crouchPressed)
+        {
+            animator.SetBool("isSteady", true); //si no pitj, està dret
+            //rb.AddForce(Vector3.forward * walkingForce, ForceMode.Force);
+
+        }
+
+        rb.AddForce(transform.forward * walkingForce * forwardInput); //Move forwad
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInput);//rotate body
+        //IN progress
+        if (forwardInput != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else {
+            animator.SetBool("isRunning", false);
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Space)) //bota
+        {
+            rb.AddForce(Vector3.up * jumpingForce, ForceMode.Impulse);
+
+            animator.SetBool("isJumping", true);
+
+        }
+
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+    }
+}
