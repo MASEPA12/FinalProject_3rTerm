6c6fd0ac5f74e3c63a8946d65ac23ca905511461
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-05-26 10:16:49 +0200

    ui_04

--- Assets/SCRIPTS/prova1UI.cs
+++ Assets/SCRIPTS/prova1UI.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class prova1UI : MonoBehaviour
 {
     public Image[] arrowArray;
+    public Image[] girlIcon;
 
     //aquests bools les necessitam per fer lock/unlock als nivells, no se exactament a quin script anirien
     public bool hasWonLev1;
@@ -14,33 +15,60 @@ public class prova1UI : MonoBehaviour
     public bool hasWonLev3;
 
     //private Button locatorButtonLev1; --> no se si el necessitam, mirar-ho
-    private Button locatorButtonLev2;
-    private Button locatorButtonLev3;
+    public Button locatorButtonLev2;
+    public Button locatorButtonLev3;
 
+    private bool arrowsAreShowing1;
+    private bool arrowsAreShowing2;
+    private bool arrowsAreShowing3;
+
+    private void Start()
+    {
+        girlIcon[1].gameObject.SetActive(false);
+        girlIcon[2].gameObject.SetActive(false);
+        girlIcon[3].gameObject.SetActive(false);
+
+        locatorButtonLev2.gameObject.SetActive(false);
+        locatorButtonLev3.gameObject.SetActive(false);
+    }
     void Update()
     {
-        if(hasWonLev1 == true)
+        //if the player has not won any level, the default will be the girl icon in the level 1
+        if(hasWonLev1 == true && arrowsAreShowing1 == false)
         {
+            girlIcon[0].gameObject.SetActive(false); 
             //has to show 4 arrows
             StartCoroutine(showArrows(4));
             locatorButtonLev2.gameObject.SetActive(true); //això ho hauriem de posar sa data persistance 
+            
+            girlIcon[1].gameObject.SetActive(true);
+            arrowsAreShowing1 = true;
         }
-        if(hasWonLev2 == true)
+        if (hasWonLev2 == true && arrowsAreShowing2 == false)
         {
+            girlIcon[1].gameObject.SetActive(false);
+
             //has to show 4 arrows
             StartCoroutine(showArrows(9));
             locatorButtonLev3.gameObject.SetActive(true);
+            
+            girlIcon[2].gameObject.SetActive(true);
+            arrowsAreShowing2 = true;
         }
-        if (hasWonLev3 == true)
+        if (hasWonLev3 == true && arrowsAreShowing3 == false)
         {
+            girlIcon[2].gameObject.SetActive(false);
+            girlIcon[3].gameObject.SetActive(true);
+
             //has to show all the arrows
-            StartCoroutine(showArrows(arrowArray.Length));
+            StartCoroutine(showArrows(arrowArray.Length-1));
+            arrowsAreShowing3 = true;
         }
     }
 
-    private IEnumerator showArrows(int i)
+    private IEnumerator showArrows(int a)
     {
-        for (i = 0; i <= 4; i++)
+        for (int i = 0; i <= a; i++)
         {
             Debug.Log("un pic");
             arrowArray[i].gameObject.SetActive(true);
